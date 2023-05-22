8cd83501822c07f6f19ddb08617c066167e46130
Kirill Radchenko <kirillalexradchenko@gmail.com>
2023-05-22 14:11:50 +0200

    feat: change text size in no todos page

--- src/components/todos/NoToDoList.tsx
+++ src/components/todos/NoToDoList.tsx
@@ -1,7 +1,7 @@
 export const NoToDoList = () => {
     return (
-        <div className="w-full h-full flex justify-center items-center">
-            <p className="text-2xl">
+        <div className="w-full h-full flex justify-center items-center p-5">
+            <p className="text-xl sm:text-2xl">
                 Oops! 😬 You haven't selected any <span className="text-purple-500">ToDo List</span>
             </p>
         </div>
